5c12d80b7e713fd147013643fad2b3a89f47dac3
Vaclav Mach <vaclavmach@hotmail.cz>
2025-08-18 15:30:44 +0200

    update to SDL3

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,28 +3,37 @@
 
 Game::Game() = default;
 
-Game::~Game() {
-    if (renderer) SDL_DestroyRenderer(renderer);
-    if (window) SDL_DestroyWindow(window);
+Game::~Game()
+{
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+    }
     SDL_Quit();
 }
 
-bool Game::init() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+bool Game::init()
+{
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\n";
         return false;
     }
 
-    window = SDL_CreateWindow("Daemonium",
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              800, 600, SDL_WINDOW_SHOWN);
-    if (!window) {
+    window = SDL_CreateWindow("Daemonium", 800, 600, 0);
+    if (!window)
+    {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
         return false;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer)
+    {
         std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << "\n";
         return false;
     }
@@ -33,24 +42,38 @@ bool Game::init() {
     return true;
 }
 
-void Game::run() {
+void Game::run()
+{
     SDL_Event e;
     int x = 100, y = 100;
 
-    while (running) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+    while (running)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_EVENT_QUIT)
+            {
                 running = false;
             }
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
+            if (e.type == SDL_EVENT_KEY_DOWN)
+            {
+                switch (e.key.key)
+                {
                 case SDLK_ESCAPE:
                     running = false;
                     break;
-                case SDLK_UP:    y -= 10; break;
-                case SDLK_DOWN:  y += 10; break;
-                case SDLK_LEFT:  x -= 10; break;
-                case SDLK_RIGHT: x += 10; break;
+                case SDLK_UP:
+                    y -= 10;
+                    break;
+                case SDLK_DOWN:
+                    y += 10;
+                    break;
+                case SDLK_LEFT:
+                    x -= 10;
+                    break;
+                case SDLK_RIGHT:
+                    x += 10;
+                    break;
                 }
             }
         }
@@ -60,7 +83,7 @@ void Game::run() {
         SDL_RenderClear(renderer);
 
         // Draw a red rectangle (player)
-        SDL_Rect rect = {x, y, 50, 50};
+        SDL_FRect rect = {(float)x, (float)y, 50.0f, 50.0f};
         SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
 
